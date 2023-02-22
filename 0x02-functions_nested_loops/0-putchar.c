b868e746e86dec38e2bbdcc0eef84ea165f60074
root <samuelfrancis90@hotmail.com>
2023-02-21 17:00:14 -0800

    Nest loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (success)
+*/
+
+/* Void function*/
+int main(void)
+{
+/*print putchar*/
+printf("_putchar\n");
+
+/*return(0)*/
+return (0);
+}
