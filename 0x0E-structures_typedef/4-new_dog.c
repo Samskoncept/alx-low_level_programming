1e58743eade200fe3db2476371450ce38d61bc74
root <samuelfrancis90@hotmail.com>
2023-03-20 17:24:41 -0700

    UPdated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,49 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
  */
-int _strlen(char *s)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
+struct dog *doge;
+int i, j, k;
+char *n, *o;
 
-	i = 0;
+doge = malloc(sizeof(struct dog));
+if (doge == NULL)
+return (NULL);
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
+for (i = 0; name[i] != '\0'; i++)
+;
+for (j = 0; owner[j] != '\0'; j++)
+;
 
-	return (i);
+n = malloc(sizeof(char) * i + 1);
+if (n == NULL)
+{
+free(doge);
+return (NULL);
+}
+o = malloc(sizeof(char) * j + 1);
+if (o == NULL)
+{
+free(n);
+free(doge);
+return (NULL);
+}
+for (k = 0; k <= i; k++)
+n[k] = name[k];
+for (k = 0; k <= j; k++)
+o[k] = owner[k];
+
+doge->name = n;
+doge->age = age;
+doge->owner = o;
+
+return (doge);
 }
